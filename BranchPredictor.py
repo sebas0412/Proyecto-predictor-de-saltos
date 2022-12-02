8ca5a25c7599e75005b6f729ebb7c22139a642a9
sebas0412 <44914799+sebas0412@users.noreply.github.com>
2022-12-02 17:55:10 -0600

    Global brach predictor

--- BranchPredictor.py
+++ BranchPredictor.py
@@ -4,71 +4,162 @@ def main():
     n = len(sys.argv)
     if(n >= 4):
         s = int(sys.argv[1])
-        bp = sys.argv[2]
-        gh = sys.argv[3]
-        lh = sys.argv[4]
+        bp = int(sys.argv[2])
+        gh = int(sys.argv[3])
+        lh = int(sys.argv[4])
         print("Bits a indexar: ", s)
         print("Tipo de predictor: ",sys.argv[2])
         print("Tamaño de registro de historia global: ",sys.argv[3])
         print("Tamaño de registro de historia local: ",sys.argv[4])
+        globalRegister = 0
         predictor =  [[0 for i in range(2)]for j in range(2**s)]
         print(len(predictor))
         
-    ##
-    with open('saltos.trace','r') as f:
-        branches = 0
-        takenHit = 0
-        takenMiss = 0
-        notTakenHit = 0
-        notTakenMiss = 0
-        
-        for line in f:
-            
-            subset = line.split()
-            address = int(subset[0])
-            jumptaken = subset[1]
+    if(bp == 1):
 
-            mask = (2**s)-1
-            res = address & mask
-            if predictor[res] == [0,0] and jumptaken == 'T':
-                notTakenMiss += 1
-                predictor[res] = [0,1]
+        with open('saltos.trace','r') as f:
+            branches = 0
+            takenHit = 0
+            takenMiss = 0
+            notTakenHit = 0
+            notTakenMiss = 0
 
-            elif predictor[res] == [0,0] and jumptaken == 'N':
-                notTakenHit += 1
-                predictor[res] = [0,0]
+            for line in f:
 
-            elif predictor[res] == [0,1] and jumptaken == 'T':
-                notTakenMiss += 1
-                predictor[res] = [1,1]
+                subset = line.split()
+                address = int(subset[0])
+                jumptaken = subset[1]
 
-            elif predictor[res] == [0,1] and jumptaken == 'N':
-                notTakenHit += 1
-                predictor[res] = [0,0]
-            
-            elif predictor[res] == [1,1] and jumptaken == 'T':
-                takenHit += 1
-                predictor[res] = [1,1]
+                mask = (2**s)-1
+                globalMask = (2**gh)-1
 
-            elif predictor[res] == [1,1] and jumptaken == 'N':
-                takenMiss += 1
-                predictor[res] = [1,0]
+                #valor actual del registro global
+                globalRegisterCurrentValue = globalRegister & globalMask
 
-            elif predictor[res] == [1,0] and jumptaken == 'T':
-                takenHit += 1
-                predictor[res] = [1,1]
+                #ultimos s bits de pc
+                indexer =address & mask
 
-            elif predictor[res] == [1,0] and jumptaken == 'N':
-                takenMiss += 1
-                predictor[res] = [0,0]
+                #Xor entre el valor del registro global y los ultimos s bits de PC
+                globalIndexer = globalRegisterCurrentValue ^ indexer
 
-        branches = takenMiss + takenHit + notTakenHit + notTakenMiss
-        print("Branches: ", branches)
-        print("Predicted taken branches: ", takenHit)
-        print("Not predicted taken branches: ", takenMiss)
-        print("Predicted not taken branches: ", notTakenHit)
-        print("Not predicted not taken branches: ", notTakenMiss)
-        print("percentage: ", (takenHit + notTakenHit) * 100/(branches))
+                if predictor[globalIndexer] == [0,0] and jumptaken == 'T':
+                    globalRegister << 1
+                    globalRegister += 1
+
+                    notTakenMiss += 1
+                    predictor[globalIndexer] = [0,1]
+
+                elif predictor[globalIndexer] == [0,0] and jumptaken == 'N':
+                    globalRegister << 1
+
+                    notTakenHit += 1
+                    predictor[globalIndexer] = [0,0]
+
+                elif predictor[globalIndexer] == [0,1] and jumptaken == 'T':
+                    globalRegister << 1
+                    globalRegister += 1
+
+                    notTakenMiss += 1
+                    predictor[globalIndexer] = [1,1]
+
+                elif predictor[globalIndexer] == [0,1] and jumptaken == 'N':
+                    globalRegister << 1
+
+                    notTakenHit += 1
+                    predictor[globalIndexer] = [0,0]
+
+                elif predictor[globalIndexer] == [1,1] and jumptaken == 'T':
+                    globalRegister << 1
+                    globalRegister += 1
+
+                    takenHit += 1
+                    predictor[globalIndexer] = [1,1]
+
+                elif predictor[globalIndexer] == [1,1] and jumptaken == 'N':
+                    globalRegister << 1
+
+                    takenMiss += 1
+                    predictor[globalIndexer] = [1,0]
+
+                elif predictor[globalIndexer] == [1,0] and jumptaken == 'T':
+                    globalRegister << 1
+                    globalRegister += 1
+
+                    takenHit += 1
+                    predictor[globalIndexer] = [1,1]
+
+                elif predictor[globalIndexer] == [1,0] and jumptaken == 'N':
+                    globalRegister << 1
+
+                    takenMiss += 1
+                    predictor[globalIndexer] = [0,0]
+
+            branches = takenMiss + takenHit + notTakenHit + notTakenMiss
+            print("Branches: ", branches)
+            print("Predicted taken branches: ", takenHit)
+            print("Not predicted taken branches: ", takenMiss)
+            print("Predicted not taken branches: ", notTakenHit)
+            print("Not predicted not taken branches: ", notTakenMiss)
+            print("percentage: ", (takenHit + notTakenHit) * 100/(branches))
+
+    elif(bp == 0):
+
+        ##Predictor de saltos de 2-bits
+        with open('saltos.trace','r') as f:
+            branches = 0
+            takenHit = 0
+            takenMiss = 0
+            notTakenHit = 0
+            notTakenMiss = 0
+
+            for line in f:
+
+                subset = line.split()
+                address = int(subset[0])
+                jumptaken = subset[1]
+
+                mask = (2**s)-1
+                indexer = address & mask
+
+                if predictor[indexer] == [0,0] and jumptaken == 'T':
+                    notTakenMiss += 1
+                    predictor[indexer] = [0,1]
+
+                elif predictor[indexer] == [0,0] and jumptaken == 'N':
+                    notTakenHit += 1
+                    predictor[indexer] = [0,0]
+
+                elif predictor[indexer] == [0,1] and jumptaken == 'T':
+                    notTakenMiss += 1
+                    predictor[indexer] = [1,1]
+
+                elif predictor[indexer] == [0,1] and jumptaken == 'N':
+                    notTakenHit += 1
+                    predictor[indexer] = [0,0]
+
+                elif predictor[indexer] == [1,1] and jumptaken == 'T':
+                    takenHit += 1
+                    predictor[indexer] = [1,1]
+
+                elif predictor[indexer] == [1,1] and jumptaken == 'N':
+                    takenMiss += 1
+                    predictor[indexer] = [1,0]
+
+                elif predictor[indexer] == [1,0] and jumptaken == 'T':
+                    takenHit += 1
+                    predictor[indexer] = [1,1]
+
+                elif predictor[indexer] == [1,0] and jumptaken == 'N':
+                    takenMiss += 1
+                    predictor[indexer] = [0,0]
+
+            branches = takenMiss + takenHit + notTakenHit + notTakenMiss
+            print("Branches: ", branches)
+            print("Predicted taken branches: ", takenHit)
+            print("Not predicted taken branches: ", takenMiss)
+            print("Predicted not taken branches: ", notTakenHit)
+            print("Not predicted not taken branches: ", notTakenMiss)
+            print("percentage: ", (takenHit + notTakenHit) * 100/(branches))
         
         
 
